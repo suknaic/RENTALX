2e197d480e5830cc991af74eb8898ab213cc6d72
suknaic <suknaicwp@gmail.com>
2021-11-07 14:18:45 -0500

    editação da interface de categories

--- src/modules/cars/repositories/ICategoryRepository.ts
+++ src/modules/cars/repositories/ICategoryRepository.ts
@@ -1,10 +1,10 @@
 import { ICreateCategoriDTO } from '../../../dtos/ICreateCategoryDTO';
-import { Category } from '../model/Category';
+import { Category } from '../entities/Category';
 
 interface ICategoryRepository {
-  findByName(name: string): Category;
-  list(): Category[];
-  create({ name, description }: ICreateCategoriDTO): void;
+  findByName(name: string): Promise<Category>;
+  list(): Promise<Category[]>;
+  create({ name, description }: ICreateCategoriDTO): Promise<void>;
 }
 
 export { ICategoryRepository };
