421769f253020c5e03cae211f4080225a2d0f1d4
suknaic <suknaicwp@gmail.com>
2021-11-22 11:54:36 -0500

    instalacao do jest e test de criacao de categoria

---
+++ src/modules/cars/repositories/Fake/FakeCategoryRepositories.ts
@@ -0,0 +1,33 @@
+import { ICreateCategoriDTO } from '../../dtos/ICreateCategoryDTO';
+import { Category } from '../../entities/Category';
+import { ICategoryRepository } from '../ICategoryRepository';
+
+class FakeCategoryRepository implements ICategoryRepository {
+  private repository: Category[];
+
+  constructor() {
+    this.repository = [];
+  }
+
+  async create({ name, description }: ICreateCategoriDTO): Promise<void> {
+    const categori = new Category();
+
+    Object.assign(categori, {
+      name,
+      description,
+    });
+    this.repository.push(categori);
+  }
+
+  async list(): Promise<Category[]> {
+    const categories = this.repository;
+    return categories;
+  }
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.repository.find((category) => category.name === name);
+    return category;
+  }
+}
+
+export { FakeCategoryRepository };
---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,39 @@
+import { AppError } from '../../../../error/AppError';
+import { FakeCategoryRepository } from '../../repositories/Fake/FakeCategoryRepositories';
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+
+let categoryRepository: FakeCategoryRepository;
+let createCategoryUseCase: CreateCategoryUseCase;
+
+describe('[CreateCategoryUseCase]', () => {
+  beforeEach(() => {
+    categoryRepository = new FakeCategoryRepository();
+    createCategoryUseCase = new CreateCategoryUseCase(categoryRepository);
+  });
+
+  it('should be able to create a new Category', async () => {
+    const category = {
+      name: 'category test',
+      description: 'description category test',
+    };
+
+    await createCategoryUseCase.execute(category);
+
+    const categoryCreated = await categoryRepository.findByName(category.name);
+
+    expect(categoryCreated).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new category with name exists', async () => {
+    const category = {
+      name: 'category test',
+      description: 'description category test',
+    };
+
+    await createCategoryUseCase.execute(category);
+
+    expect(createCategoryUseCase.execute(category)).rejects.toBeInstanceOf(
+      AppError
+    );
+  });
+});
