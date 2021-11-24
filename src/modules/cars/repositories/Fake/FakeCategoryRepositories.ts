9b7f36d06beadf36a3e711ffa04271de78e6522a
suknaic <suknaicwp@gmail.com>
2021-11-24 10:38:03 -0500

    alterando importacoes dos repositorios de test

--- src/modules/cars/repositories/Fake/FakeCategoryRepositories.ts
+++ src/modules/cars/repositories/Fake/FakeCategoryRepositories.ts
@@ -1,5 +1,6 @@
+import { Category } from '@modules/cars/infra/typeorm/entities/Category';
+
 import { ICreateCategoriDTO } from '../../dtos/ICreateCategoryDTO';
-import { Category } from '../../entities/Category';
 import { ICategoryRepository } from '../ICategoryRepository';
 
 class FakeCategoryRepository implements ICategoryRepository {
---
+++ src/shared/infra/typeorm/migrations/1637615743623-CreateCars.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateCars1637615743623 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
